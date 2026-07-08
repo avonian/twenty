52a5eed5b3f28a8226ef39a3fdb3057a7dce7c85
Sebastian Arango <sebastianarango201316@hotmail.com>
2026-07-08 15:39:45 +0200

    Listerner Latest event to country

--- packages/twenty-server/src/modules/modules.module.ts
+++ packages/twenty-server/src/modules/modules.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { CalendarModule } from 'src/modules/calendar/calendar.module';
 import { ConnectedAccountModule } from 'src/modules/connected-account/connected-account.module';
+import { FlamagasDerivedFieldsModule } from 'src/modules/flamagas-derived-fields/flamagas-derived-fields.module';
 import { MessagingModule } from 'src/modules/messaging/messaging.module';
 import { FieldCommentNoteLifecycleModule } from 'src/modules/note/field-comment-note-lifecycle.module';
 import { WorkflowModule } from 'src/modules/workflow/workflow.module';
@@ -15,6 +16,7 @@ import { WorkspaceMemberModule } from 'src/modules/workspace-member/workspace-me
     WorkflowModule,
     WorkspaceMemberModule,
     FieldCommentNoteLifecycleModule,
+    FlamagasDerivedFieldsModule,
   ],
   providers: [],
   exports: [],
